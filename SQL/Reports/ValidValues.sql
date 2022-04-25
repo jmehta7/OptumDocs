ba2d06495fb86c0f8f47ddad79fda58d32294287
jmehta7 <jatin.-.mehta@optum.com>
2022-04-25 09:55:31 +0530

    docs

---
+++ SQL/Reports/ValidValues.sql
@@ -0,0 +1,10 @@
+
+
+Insert Into CS.ValidValues(ValidValueID,ValidValueTypeID,ValidValueText, ValidValueOrder, IsUnanswered)
+Select 1641,257, '[Select One]',1,'Y'
+
+Insert Into CS.ValidValues(ValidValueID,ValidValueTypeID,ValidValueText, ValidValueOrder, IsUnanswered)
+Select 1642,258, '[Select One]',1,'Y'
+
+
+ 
